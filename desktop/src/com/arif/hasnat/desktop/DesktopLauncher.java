b6c3a0635e2ccb8d7d58aadbc1a7ea92667165cd
arif hasnat <arifhasnat.info@gmail.com>
2016-01-28 05:03:28 +0600

    Background

---
+++ desktop/src/com/arif/hasnat/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.arif.hasnat.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.arif.hasnat.MainActivity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainActivity(), config);
+	}
+}
