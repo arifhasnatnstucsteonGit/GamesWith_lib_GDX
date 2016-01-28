9ae6beb5f21926e1f9319fa73fa9cc8ce1a9955b
arif hasnat <arifhasnat.info@gmail.com>
2016-01-28 06:33:32 +0600

    sound add

--- core/src/com/arif/hasnat/MainActivity.java
+++ core/src/com/arif/hasnat/MainActivity.java
@@ -2,6 +2,7 @@ package com.arif.hasnat;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,11 +10,22 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MainActivity extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	private Music audioBG;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("bird8.jpg");
+
+
+
+		// Load Sound
+		audioBG = Gdx.audio.newMusic(Gdx.files.internal("background.mp3"));
+
+		// start the playback of the background music immediately
+		audioBG.setLooping(true);
+		// Play
+		audioBG.play();
 	}
 
 	@Override
